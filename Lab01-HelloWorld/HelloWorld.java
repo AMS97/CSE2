3d9b780414f6b71db5c841744ab1775e6edf57cd
ams97 <ams937@gmail.com>
2015-09-01 19:28:16 +0000

    First HelloWorld and Welcome Class

---
+++ Lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+
+//Adam Smith 
+//CSE-002 Lab 8/28/2015
+//LE BLAH BLAH Bonjour Monde Java programme 
+//The Class:
+public class HelloWorld{
+    //main method:
+    public static void main(String[] args) {
+        
+        // print the thing we are supposed to print
+        System.out.println("###########################################");
+        System.out.println("###########################################");
+        System.out.println("#                                         #");
+        System.out.println("#   He110 W0r1D 2015! CSE Class is fun.   #");
+        System.out.println("#                                         #");
+        System.out.println("###########################################");
+        System.out.println("###########################################");
+        System.out.println("This is not a test, it is a lab.");
+        
+        
+    }
+}
---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+//Adam Smith 
+//CSE-002 hw01 8/31/2015
+//First homework assignment
+//The Class:
+public class WelcomeClass{
+    //main method:
+    public static void main (String[] args) {
+        // Display a welcome sign signed with your Lehigh network identification and followed by a short Bio
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-A--M--S--2--1--9->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("During senior week I stayed in a yurt with some friends");
+        System.out.println("and I did the OAP PreLUsion two weeks ago.");
+        System.out.println("In between I worked at a Crab Resturant in Maryland.");
+        // That wasn't hard, now I just hope it compiles first time.
+        // it didn't compile first time
+        // =(
+        
+    }
+}    
